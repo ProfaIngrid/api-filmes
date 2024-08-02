0765a6bc3a115613154754b8c87040e0965fd577
ProfaIngrid <ingrid.bconceicao@senacsp.edu.br>
2024-08-02 08:44:59 -0300

    01/08/24

--- routes/usuariosRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuariosController = require('../controllers/usuariosController');
-
-router.use('/usuarios', usuariosController);
-
-module.exports = router;
-
